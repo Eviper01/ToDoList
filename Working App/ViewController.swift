2d2068c9b58eb31848e7de45789fe81d051ef51b
Eviper01 <19walj@mbpr147c1bc149a.gfc.internal>
2016-09-09 11:25:25 +0800

    Initial Commit

---
+++ Working App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Working App
+//
+//  Created by John Wallis on 9/09/2016.
+//  Copyright © 2016 John Wallis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
